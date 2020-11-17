49e089dbcd45ac48d0212508bff9b357b0c2e3bc
George Matheus <georgematheuslima@outlook.com>
2020-11-17 16:31:35 -0300

    Atualização pesquisas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from tabulate import tabulate
 from tarefas import Tarefas
 from datas import Data
+from pesquisas import Pesquisa
 from random import randint
 import pandas as pd
 
@@ -9,25 +10,20 @@ def menu_opcoes():
     table = [["Adicionar Nova Tarefa: ", "1"],
              ["Realizar Pesquisa: ", "2"],
              ["Ver tarefas cadastradas no Sistema: ", "3"]]
-    apresentar_menu = print(tabulate(table))
-    return apresentar_menu
-
+    print(tabulate(table))
 
 def opcoes():
     opcao_menu = int(input('Digite a opção desejada: '))
 
     if opcao_menu == 1:
-
-        # identificação numérica da Tarefa a ser cadastrada, dada de forma aleatória ( entre 0 e 100).
         identificacao_numerica_aletoria = randint(1, 100)
 
-        # Descrição da nova tarefa a ser cadastrada.
         descricao_da_tarefa = input('Descrição da tarefa: ')
 
-        # Dia em que a tarefa está sendo cadastrada.
         dia_do_cadastro = Data.dia_atual()
 
         # Prazo de execução da tarefa
+        print('Insira a data de Execução da tarefa de acordo com o for pedido.')
         dia = int(input('Digite o dia da execução: '))
         mes = int(input('Digite o mês: '))
         ano = int(input('Digite o Ano: '))
@@ -68,36 +64,28 @@ def opcoes():
 
     elif opcao_menu == 2:
         print('Realizar pesquisa.\n')
-        tipo_da_pesquisa = int(input('Digite [1] para pesquisar por Status,'
-                                     ' ou [2] para pesquisar por identificação. '))
+        Pesquisa.apresentar_tarefas()
 
         # PESQUISA POR STATUS DA TAREFA
-        if tipo_da_pesquisa == 1:
-            pesquisar = pd.read_csv("tarefas.csv")
-            apresentar_colunas = pd.DataFrame(data=pesquisar, columns=['Descrição', 'Status'])
-            print(apresentar_colunas)
+        indice_da_pesquisa = int(input('Digite o Índice da tarefa que deseja verificar: '))
+        Pesquisa.pesquisa_tarefas(indice_da_pesquisa)
 
-            # Alterar o Status de alguma Tarefa:
-            altera_status = input('Deseja alterar o status de alguma tarefa(SIM/NÃO): ')
-            if altera_status.upper() == 'SIM':
-                indice_tarefa = int(input('Digite o índice ao lado da tarefa: '))
-                linha = apresentar_colunas.loc[indice_tarefa]  # seleciona a linha do arquivo csv escolhido pelo usuário
-                # status_tarefa =
-                print(linha)
+        # Alterar o Status de alguma Tarefa:
+        altera_status = input('Deseja alterar o status de alguma tarefa(SIM/NÃO): ')
+        if altera_status.upper() == 'SIM':
+            indice_tarefa = int(input('Digite o índice ao lado da tarefa: '))
+            #linha = apresentar_colunas.loc[indice_tarefa]  # seleciona a linha do arquivo csv escolhido pelo usuário
+            # status_tarefa =
+            #print(linha)
 
-            # Alterar algum dado da lista
-            alteracao = input('Deseja alterar alguma Tarefa?:')
-            if alteracao.upper() == 'SIM':
-                print(pesquisar['identificação'])
-                indice = int(input('Digite o número de identificação da Tarefa: '))
+        # Alterar algum dado da lista
+        alteracao = input('Deseja alterar alguma Tarefa?:')
+        if alteracao.upper() == 'SIM':
+            print(pesquisar_tarefas['identificação'])
+            indice = int(input('Digite o número de identificação da Tarefa: '))
 
-            else:
-                pass
-
-        # PESQUISA POR IDENTIFICAÇÃO NUMÉRICA DA TAREFA.
-        elif tipo_da_pesquisa == 2:
-            pesquisar = pd.read_csv("tarefas.csv")
-            print(pesquisar['identificação'])
+        else:
+            pass
 
 
     elif opcao_menu == 3:
--- pesquisas.py
+++ pesquisas.py
@@ -1,14 +1,21 @@
+import pandas as pd
+
+
 class Pesquisa:
 
-    def __init__(self, opcao):
+    def __init__(self, opcao, pesquisar_tarefas, apresentar_colunas):
         self.opcao = opcao
+        self.pesquisar_tarefas = pesquisar_tarefas
+        self.apresentar_colunas = apresentar_colunas
 
-    @property
-    def altera_status(self):
-        if self == 'sim'.upper():
-            return True
+    @staticmethod
+    def apresentar_tarefas():
+        pesquisar_tarefas = pd.read_csv("tarefas.csv")
+        apresentar_colunas = pd.DataFrame(data=pesquisar_tarefas, columns=['Descrição', 'Status'])
+        print(apresentar_colunas)
 
-        elif self == 'não'.upper():
-            return False
-        else:
-            raise ValueError('Valor inválido')
+    def pesquisa_tarefas(self):
+        pesquisar_tarefas = pd.read_csv("tarefas.csv")
+        indice_da_pesquisa = (self)
+        apresentar_tarefa = pesquisar_tarefas.loc[indice_da_pesquisa]
+        print(apresentar_tarefa)
